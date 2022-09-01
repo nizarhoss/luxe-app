f399a8ce6adecb08cd49d43a8235822bea360364
Nizar Hoss <nizarhoss@Nizars-MacBook.local>
2022-09-01 00:36:32 -0700

    first commit

---
+++ components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>Luxe's Products are licensed to be sold by 1325088 BC LTD. 
+      This company authorized to sell our products with their certified GS1 barcodes  </div>
+  )
+}
+
+export default Footer
---
+++ components/FooterBanner.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const FooterBanner = () => {
+  return (
+    <div>Luxe's Products are licensed to be sold by 1325088 BC LTD. 
+    This company is authorized to sell Luxe's products with their certified GS1 barcodes</div>
+  )
+}
+
+export default FooterBanner
